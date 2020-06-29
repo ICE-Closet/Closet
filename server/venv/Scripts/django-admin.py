014f1517d1f3722cbe0516e5373ec0d6e633f359
ujin2021 <ujin2021@naver.com>
2020-06-29 21:00:08 +0900

    install, start project and app

---
+++ server/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ujin2\desktop\2020_proejct\project_closet\closet\server\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

24bebe73f5ca9be4f2dd9532ba799f41bea574bd
JaesungLeee <viki0114@naver.com>
2020-08-26 23:35:41 +0900

    Update UI : Add all fragments with chipNavigationBar

---
+++ SMARTICE_Closet/app/src/main/java/com/example/smartice_closet/fragments/closetFragment.kt
@@ -0,0 +1,24 @@
+package com.example.smartice_closet.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.smartice_closet.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [closetFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class closetFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_closet, container, false)
+    }
+
+}
--- SMARTICE_Closet/app/src/main/java/com/example/smartice_closet/main.kt
+++ SMARTICE_Closet/app/src/main/java/com/example/smartice_closet/main.kt
@@ -3,7 +3,10 @@ package com.example.smartice_closet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.smartice_closet.fragments.closetFragment
 import com.example.smartice_closet.fragments.homeFragment
+import com.example.smartice_closet.fragments.likeFragment
 import com.example.smartice_closet.fragments.profileFragment
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,19 +17,53 @@ class main : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val homeFragment = homeFragment()
+        val likeFragment = likeFragment()
+        val closetFragment = closetFragment()
         val profileFragment = profileFragment()
 
-        makeCurrentFragment(homeFragment)
+        val chipNavigationBar : ChipNavigationBar = findViewById(R.id.bottom_navBar)
+        chipNavigationBar.setItemSelected(R.id.home, true)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
 
-        bottom_navBar.setOnItemSelectedListener {
-            // Not Implemented
-        }
-    }
+        chipNavigationBar.setOnItemSelectedListener { id ->
+            when(id) {
+                R.id.home -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
 
-    private fun makeCurrentFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frame_layout, fragment)
-            commit()
+                R.id.closet -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, closetFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.like -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, likeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.profile -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, profileFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+            true
         }
     }
 }

935570162dcff66fd51649594b10c0231b0fc832
JaesungLeee <viki0114@naver.com>
2020-09-16 20:55:08 +0900

    Update UI : Add Recommend Activity

---
+++ SMARTICE_Closet/app/src/main/java/com/example/smartice_closet/recommend.kt
@@ -0,0 +1,11 @@
+package com.example.smartice_closet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recommend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommend)
+    }
+}
